77cb662636c3f53dc44d95a6ac4b528974c49cd8
Automation Test Engineer(PK) <parvkhanna27@gmail.com>
2024-04-04 15:29:55 +0530

    Added new methods to Expression execution

--- Basics/ExpressionExecution.py
+++ Basics/ExpressionExecution.py
@@ -12,4 +12,18 @@ print(a*Text*b)
 a, b = "2" , 3
 txt = "@"
 
-print((a+txt)*b)
+print((a+txt)*b)    # output  [  2@2@2@  ]
+
+#  Numeric values can operate with all arthemetic operators
+
+a,b = 2,3
+c = 4 
+print(a+b*c)   # output   [  14  ]
+
+
+
+#  Arthemtic expression with integer and float with result in float
+
+a,b = 10, 5.0
+c = a*b
+print(c)  
